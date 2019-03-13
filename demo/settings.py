d8e0540503af853072ae527f734992c4d3a1144b
limeiyang <1730422101@qq.com>
2019-03-13 14:09:30 +0800

    静态文件处理（本地图片加载）

--- demo/settings.py
+++ demo/settings.py
@@ -118,4 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+
+# 添加本地图片配置
+STATIC_URL = '/stt/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
--- demo/urls.py
+++ demo/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from joke import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('joke/index',views.index),
 	path('home/',views.home),
+
 ]
+# 添加本地图片的配置
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
